6d085dc6d81a4360662062ce575a415f52b08ae3
Keller Jordan <kellerjordan1998@gmail.com>
2018-01-31 07:23:23 +0000

    write class wrapper around TriMap, improve animation file

--- animate.py
+++ animate.py
@@ -4,21 +4,50 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from matplotlib.animation import FuncAnimation
+import torch
+from torchvision import datasets, transforms
+
+import argparse
 import sys
+import pickle
 
-import trimap
+from trimap import Wrapper as TriMap
 
-def record_gif(optimizer):
+
+def save_viz(dataset='mnist2500', optim='sgd', lr=None, animated=True):
     
-    X = np.loadtxt('data/mnist2500_X.txt')
-    labels = np.loadtxt('data/mnist2500_labels.txt')
+    if dataset == 'mnist2500':
+        X = np.loadtxt('data/%s_X.txt' % dataset)
+        labels = np.loadtxt('data/%s_labels.txt' % dataset)
+    elif dataset == 'mnist60000':
+        tsfm = transforms.Compose([transforms.ToTensor()])
+        mnist = datasets.MNIST('data', train=True, download=True, transform=tsfm)
+        X = (mnist.train_data.type(torch.FloatTensor) / 255.0).view(-1, 784).numpy()
+        labels = mnist.train_labels.numpy()
     
-    print('Computing TriMap embedding using %s optimizer' % optimizer)
-    Y, Y_seq = trimap.embed(X, optimizer=optimizer, return_seq=True, verbose=True)
+    print('Computing TriMap embedding using %s optimizer...' % optim)
+
+    trimap = TriMap(X)
+    
+    triplets_path = 'models/%s_triplets.pkl' % dataset
+    
+    try:
+        with open(triplets_path, 'rb') as f:
+            triplets_init = pickle.load(f)
+            
+        trimap.triplets, trimap.weights = triplets_init
+    except:
+        trimap.generate_triplets(verbose=True)
+        triplets_state = (trimap.triplets, trimap.weights)
+        
+        with open(triplets_path, 'wb') as f:
+            pickle.dump(triplets_state, f)
+        
+    Y_seq = trimap.embed(num_iters=1000, optimizer=optim, return_seq=True, verbose=True)
     
     fig, ax = plt.subplots()
     scatter = ax.scatter([], [], 20, [])
-    ax.set_title('%s (epoch 0)' % optimizer)
+    ax.set_title('%s (epoch 0)' % optim)
 
     def init():
         return scatter,
@@ -28,15 +57,30 @@ def record_gif(optimizer):
             print('Animating, iteration %d / %d' % (i, len(Y_seq)))
         ax.clear()
         ax.scatter(Y_seq[i][:, 0], Y_seq[i][:, 1], 20, labels)
-        ax.set_title('%s (epoch %d)' % (optimizer, i))
+        ax.set_title('%s (epoch %d)' % (optim, i))
         return ax, scatter
     
-    anim = FuncAnimation(fig, update, init_func=init,
-                     frames=len(Y_seq), interval=50)
-    path = 'animations/%s.gif' % optimizer
-    print('Saving animation as %s' % path)
-    anim.save(path, writer='imagemagick', fps=30)
+    if animated:
+        anim = FuncAnimation(fig, update, init_func=init,
+                         frames=len(Y_seq), interval=50)
+        path = 'animations/%s.gif' % optim
+        print('Saving animation as %s' % path)
+        anim.save(path, writer='imagemagick', fps=30)
+    
+    else:
+        Y = Y_seq[-1]
+        ax.clear()
+        ax.scatter(Y[:, 0], Y[:, 1], 20, labels)
+        ax.set_title('%s' % optim)
+        path = 'animations/%s.png' % optim
+        print('Saving figure as %s' % path)
+        plt.savefig(path)
+        
     
 if __name__ == '__main__':
-    optimizer = sys.argv[1]
-    record_gif(optimizer)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, default='mnist2500')
+    parser.add_argument('--optimizer', type=str, default='sgd')
+    parser.add_argument('--lr', type=float, default=1.0)
+    args = parser.parse_args()
+    save_viz(args.dataset, args.optimizer, args.lr)
--- trimap.py
+++ trimap.py
@@ -5,6 +5,7 @@ pytorch trimap implementation
 
 from sklearn.neighbors import NearestNeighbors as knn
 import numpy as np
+import pickle
 
 
 import torch
@@ -12,10 +13,10 @@ from torch import nn, optim
 from torch.autograd import Variable
 
 
-class TriMap(nn.Module):
+class TriMapper(nn.Module):
     
     def __init__(self, triplets, weights, out_shape):
-        super(TriMap, self).__init__()
+        super(TriMapper, self).__init__()
         n, num_dims = out_shape
         self.Y = nn.Embedding(n, num_dims, sparse=False)
         for W in self.Y.parameters():
@@ -36,133 +37,140 @@ class TriMap(nn.Module):
         return self.Y._parameters['weight'].cpu().data.numpy()
 
 
-def embed(X, kin = 50, initial_dims=50, max_iter=2000, optimizer='sgd',
-          return_seq=False, verbose=False):
+class Wrapper:
     
-    triplets, weights = generate_triplets(X, kin = kin, verbose=verbose)
-
-    num_examples = X.shape[0]
-    num_triplets = triplets.shape[0]
-    model = TriMap(triplets, weights, out_shape=[num_examples, 2])
-    model.cuda()
-
-    tol = 1e-7
-    C = np.inf
-
-    eta = 1000.0 / num_triplets * num_examples
-    
-    if optimizer == 'sgd':
-        optimizer = optim.SGD(model.parameters(), lr=eta)
-    elif optimizer == 'sgd-momentum':
-        optimizer = optim.SGD(model.parameters(), lr=eta, momentum=.9)
-    elif optimizer == 'adam':
-        optimizer = optim.Adam(model.parameters())
-    elif optimizer == 'adadelta':
-        optimizer = optim.Adadelta(model.parameters())
-    elif optimizer == 'rmsprop':
-        optimizer = optim.RMSprop(model.parameters())
-    
-    if return_seq:
-        Y_seq = []
-
-    for i in range(max_iter):
-        old_C = C
-
-        loss, num_viol = model()
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        C = loss.data.cpu().numpy()
-        viol = float(num_viol) / num_triplets
-
-        if old_C > C + tol:
-            eta = eta * 1.01
-        else:
-            eta = eta * 0.5
-        optimizer.param_groups[0]['lr'] = eta
+    def __init__(self, X):
+        self.X = X
         
-        if return_seq and i < 500:
-            Y = model.get_embeddings()
-            Y_seq.append(Y)
+    def embed(self, num_iters=2000, optimizer='sgd', lr=None,
+              return_seq=False, verbose=False):
 
-        if verbose and (i+1) % 100 == 0:
-            print('Iteration: %4d, Loss: %3.3f, Violated triplets: %0.4f' % (i+1, loss, viol))
-            
-    Y = model.get_embeddings()
-    
-    if return_seq:
-        return Y, Y_seq
-    
-    return Y
+        num_examples = self.X.shape[0]
+        num_triplets = self.triplets.shape[0]
+        model = TriMapper(self.triplets, self.weights, out_shape=[num_examples, 2])
+        model.cuda()
 
+        tol = 1e-7
+        C = np.inf
 
-"""
-generate_triplets()
-Created on Sat May 27 12:46:25 2017
+        if lr == None:
+            eta = 1000.0 / num_triplets * num_examples
 
-@author: ehsanamid
-"""
-def generate_triplets(X, kin=50, kout=10, kr=5, weight_adj=False, random_triplets=True, verbose=False):
-    num_extra = np.maximum(kin+50, 60) # look up more neighbors
-    # ^ ???
-    n = X.shape[0]
-    nbrs = knn(n_neighbors= num_extra + 1, algorithm='auto').fit(X)
-    distances, indices = nbrs.kneighbors(X)
-#    sig = distances[:,10]
-    sig = np.maximum(np.mean(distances[:, 10:20], axis=1), 1e-20) # scale parameter
-    P = np.exp(-distances**2/np.reshape(sig[indices.flatten()],[n, num_extra + 1])/sig[:, np.newaxis])
-    sort_indices = np.argsort(-P, axis = 1) # actual neighbors
+        if optimizer == 'sgd':
+            optimizer = optim.SGD(model.parameters(), lr=eta)
+        elif optimizer == 'sgd-momentum':
+            optimizer = optim.SGD(model.parameters(), lr=eta, momentum=.9)
+        elif optimizer == 'adam':
+            optimizer = optim.Adam(model.parameters())
+        elif optimizer == 'adadelta':
+            optimizer = optim.Adadelta(model.parameters(), lr=eta)
+        elif optimizer == 'rmsprop':
+            optimizer = optim.RMSprop(model.parameters())
+
+        if return_seq:
+            Y_seq = []
+
+        for i in range(num_iters):
+            old_C = C
+
+            loss, num_viol = model()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            C = loss.data.cpu().numpy()
+            viol = float(num_viol) / num_triplets
+
+            if optimizer in ['sgd', 'sgd-momentum']:
+                if old_C > C + tol:
+                    eta = eta * 1.01
+                else:
+                    eta = eta * 0.5
+                optimizer.param_groups[0]['lr'] = eta
+
+            if return_seq:
+                Y = model.get_embeddings()
+                Y_seq.append(Y)
+
+            if verbose and (i+1) % 100 == 0:
+                print('Iteration: %4d, Loss: %3.3f, Violated triplets: %0.4f' % (i+1, loss, viol))
+
+        Y = model.get_embeddings()
+
+        return Y_seq if return_seq else Y
     
-    triplets = np.zeros([n * kin * kout, 3], dtype=np.int32)
-    weights = np.zeros(n * kin * kout)
-    
-    cnt = 0
-    for i in range(n):
-        for j in range(kin):
-            sim = indices[i,sort_indices[i, j+1]]
-            p_sim = P[i,sort_indices[i, j+1]]
-            rem = indices[i,sort_indices[i, :j+2]].tolist()
-            l = 0
-            while (l < kout):
-                out = np.random.choice(n)
-                if out not in rem:
-                    triplets[cnt] = [i, sim, out]
-                    p_out = max(np.exp(-np.sum((X[i] - X[out])**2) / (sig[i] * sig[out])), 1e-20)
-                    weights[cnt] = p_sim / p_out
-                    rem.append(out)
-                    l += 1
-                    cnt += 1
-        if verbose and (i+1) % 500 == 0:
-            print('Generated triplets %d / %d' % (i+1, n))
-    if random_triplets:
-#         kr = 5
-        triplets_rand = np.zeros([n * kr, 3])
-        weights_rand = np.zeros(n * kr)
+    def generate_triplets(self, kin=50, kout=10, kr=5,
+                          weight_adj=False, random_triplets=True, verbose=False):
+        """
+        generate_triplets()
+        Created on Sat May 27 12:46:25 2017
+
+        @author: ehsanamid
+        """
+        
+        X = self.X
+        
+        num_extra = np.maximum(kin+50, 60) # look up more neighbors
+        # ^ ???
+        n = X.shape[0]
+        nbrs = knn(n_neighbors= num_extra + 1, algorithm='auto').fit(X)
+        distances, indices = nbrs.kneighbors(X)
+        sig = np.maximum(np.mean(distances[:, 10:20], axis=1), 1e-20) # scale parameter
+        P = np.exp(-distances**2/np.reshape(sig[indices.flatten()],[n, num_extra + 1])/sig[:, np.newaxis])
+        sort_indices = np.argsort(-P, axis = 1) # actual neighbors
+
+        triplets = np.zeros([n * kin * kout, 3], dtype=np.int32)
+        weights = np.zeros(n * kin * kout)
+
+        cnt = 0
         for i in range(n):
-            cnt = 0
-            while cnt < kr:
-                sim = np.random.choice(n)
-                out = np.random.choice(n)
-                if sim == i or out == i or out == sim:
-                    continue
-                p_sim = max(np.exp(-np.sum((X[i]-X[sim])**2)/(sig[i] * sig[sim])), 1e-20)
-                p_out = max(np.exp(-np.sum((X[i]-X[out])**2)/(sig[i] * sig[out])), 1e-20)
-                if p_sim < p_out:
-                    sim, out = out, sim
-                    p_sim, p_out = p_out, p_sim
-                triplets_rand[i * kr + cnt] = [i, sim, out]
-                weights_rand[i * kr + cnt] = p_sim / p_out
-                cnt += 1
+            for j in range(kin):
+                sim = indices[i,sort_indices[i, j+1]]
+                p_sim = P[i,sort_indices[i, j+1]]
+                rem = indices[i,sort_indices[i, :j+2]].tolist()
+                l = 0
+                while (l < kout):
+                    out = np.random.choice(n)
+                    if out not in rem:
+                        triplets[cnt] = [i, sim, out]
+                        p_out = max(np.exp(-np.sum((X[i] - X[out])**2) / (sig[i] * sig[out])), 1e-20)
+                        weights[cnt] = p_sim / p_out
+                        rem.append(out)
+                        l += 1
+                        cnt += 1
             if verbose and (i+1) % 500 == 0:
-                print('Generated random triplets %d / %d' % (i+1, n))
-        triplets = np.vstack((triplets, triplets_rand))
-        weights = np.hstack((weights, weights_rand))
-    triplets = triplets[~np.isnan(weights)]
-    weights = weights[~np.isnan(weights)]
-    weights /= np.max(weights)
-    weights += 0.0001
-    if weight_adj:
-        weights = np.log(1 + 50 * weights)
+                print('Generated triplets %d / %d' % (i+1, n))
+        if random_triplets:
+    #         kr = 5
+            triplets_rand = np.zeros([n * kr, 3])
+            weights_rand = np.zeros(n * kr)
+            for i in range(n):
+                cnt = 0
+                while cnt < kr:
+                    sim = np.random.choice(n)
+                    out = np.random.choice(n)
+                    if sim == i or out == i or out == sim:
+                        continue
+                    p_sim = max(np.exp(-np.sum((X[i]-X[sim])**2)/(sig[i] * sig[sim])), 1e-20)
+                    p_out = max(np.exp(-np.sum((X[i]-X[out])**2)/(sig[i] * sig[out])), 1e-20)
+                    if p_sim < p_out:
+                        sim, out = out, sim
+                        p_sim, p_out = p_out, p_sim
+                    triplets_rand[i * kr + cnt] = [i, sim, out]
+                    weights_rand[i * kr + cnt] = p_sim / p_out
+                    cnt += 1
+                if verbose and (i+1) % 500 == 0:
+                    print('Generated random triplets %d / %d' % (i+1, n))
+            triplets = np.vstack((triplets, triplets_rand))
+            weights = np.hstack((weights, weights_rand))
+        triplets = triplets[~np.isnan(weights)]
+        weights = weights[~np.isnan(weights)]
         weights /= np.max(weights)
-    return triplets, weights
+        weights += 0.0001
+        if weight_adj:
+            weights = np.log(1 + 50 * weights)
+            weights /= np.max(weights)
+        
+        self.triplets = triplets
+        self.weights = weights
+        # do some pickling
